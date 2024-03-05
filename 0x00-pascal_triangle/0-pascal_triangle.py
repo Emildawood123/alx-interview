c78b671030e2688d62032ca1d8f2e5e95c62c8e1
emil <edawood997@gmail.com>
2024-03-05 15:00:40 +0200

    done

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+def pascal_triangle(n):
+    all = []
+    temp = []
+    if n <= 0:
+        return []
+    else:
+        for i in range(0, n):
+            new = []
+            for j in range(0, i+1):
+                if j == 0 or j == i:
+                    new.append(1)
+                else:
+                    new.append(temp[j] + temp[j - 1])
+            temp = new
+            print(temp)
+            all.append(new)
+        return all
