1ee947204a3df323f9b53a4468ae34f6caabc4eb
Emildawood123 <edawood997@gmail.com>
2024-03-13 12:28:54 -0400

    recursive to know can i open all boxes or no

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" open all boxes """
+
+
+def canUnlockAll(boxes):
+    """canUnlockAll"""
+    boxes_status = [0] * len(boxes)
+    boxes_status[0] = 1
+    is_have_all_keys = recursion_function(boxes, boxes[0], boxes_status)
+    return is_have_all_keys
+
+
+def recursion_function(boxes, unlockBox, boxes_status):
+    """recursion_function"""
+    if len(unlockBox) == 0 and 0 in boxes_status:
+        return False
+    elif len(unlockBox) == 0 and 0 not in boxes_status:
+        return True
+    new = []
+    for key in unlockBox:
+        if boxes_status[key] == 0:
+            new = new + boxes[key]
+            boxes_status[key] = 1
+    return recursion_function(boxes, new, boxes_status)
