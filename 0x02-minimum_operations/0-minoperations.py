82fc3bc649b02cca2f59ed9ecd5abae67e2bac77
Emildawood123 <edawood997@gmail.com>
2024-03-19 08:18:34 -0400

    done isa

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,21 +7,18 @@ def minOperations(n):
     """minOperations functions"""
     if n <= 0 or n == 1:
         return 0
-    if n % 2 != 0:
-        i = 3
+    if True:
+        i = 2
         while i < n:
             if n % i == 0:
-                return int((n / i) + i)
+                print(i)
+                remind = i
             i += 1
-    biggets_reminder_equal_zero = 2
-    while n % biggets_reminder_equal_zero == 0:
-        biggets_reminder_equal_zero *= 2
-    biggets_reminder_equal_zero = biggets_reminder_equal_zero // 2
-    opertions = int(math.sqrt(biggets_reminder_equal_zero) * 2)
-    base = biggets_reminder_equal_zero
-    if biggets_reminder_equal_zero != n:
-        opertions += 1
-    while biggets_reminder_equal_zero != n:
-        biggets_reminder_equal_zero += base
-        opertions = opertions + 1
-    return opertions
+        try:
+            if remind:
+                pass
+        except Exception:
+            return n
+        if remind % 2 == 0:
+            remind = remind / 2
+        return (n / remind) + remind
