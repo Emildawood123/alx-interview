4cea04a1cd601f40ca40f447221c622e47145bd9
Emildawood123 <edawood997@gmail.com>
2024-03-19 07:30:14 -0400

    isa last try

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,10 +6,20 @@ import math
 def minOperations(n):
     """minOperations functions"""
     i = 0
-    if n >= 0 or n == 1:
+    if n <= 0 or n == 1:
         return 0
     if n % 2 != 0:
-        return n
+        b_r_e_z = 3
+        while n % b_r_e_z != 0 and b_r_e_z != n:
+            b_r_e_z += 1
+        operations = b_r_e_z
+        base = b_r_e_z
+        if n == b_r_e_z:
+            return n
+        while n != b_r_e_z:
+            operations += 1
+            b_r_e_z += base
+        return operations
     biggets_reminder_equal_zero = 2
     while n % biggets_reminder_equal_zero == 0:
         biggets_reminder_equal_zero *= 2
