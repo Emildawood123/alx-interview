f2a3b3bfaa9a62b20c6f710a783803bfbe31eae2
Emildawood123 <edawood997@gmail.com>
2024-03-19 08:23:34 -0400

    done isa

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,20 +5,19 @@ import math
 
 def minOperations(n):
     """minOperations functions"""
-    if n <= 0 or n == 1:
+    if n <= 1:
         return 0
-    if True:
-        i = 2
-        while i < n:
-            if n % i == 0:
-                print(i)
-                remind = i
-            i += 1
-        try:
-            if remind:
-                pass
-        except Exception:
-            return n
-        if remind % 2 == 0:
-            remind = remind / 2
-        return (n / remind) + remind
+    i = 2
+    while i < n:
+        if n % i == 0:
+            print(i)
+            remind = i
+        i += 1
+    try:
+        if remind:
+            pass
+    except Exception:
+        return n
+    if remind % 2 == 0:
+        remind = remind / 2
+    return int((n / remind) + remind)
