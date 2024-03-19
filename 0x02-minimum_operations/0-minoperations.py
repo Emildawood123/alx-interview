dad765957345c380e81d05cc1ff03ca5cc9ee1a9
Emildawood123 <edawood997@gmail.com>
2024-03-19 08:53:56 -0400

    Done

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
-"""0-minoperations.py"""
+"""minOperations functions"""
+import math
 
 
 def minOperations(n):
-    """minOperations"""
+    """minOperations functions"""
     if n <= 1:
         return 0
+    i = 2
+    while i < n:
+        if n % i == 0:
+            remind = i
+        i += 1
+    try:
+        if remind:
+            pass
+    except Exception:
+        return n
+    while remind % 2 == 0 and remind != 2:
+        remind = remind / 2
+    return int((n / remind) + remind)
 
-    operations = 0
-    divisor = 2
 
-    while n > 1:
-        while n % divisor == 0:
-            operations += divisor
-            n //= divisor
-        divisor += 1
-
-    return operations
+n = 40
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
