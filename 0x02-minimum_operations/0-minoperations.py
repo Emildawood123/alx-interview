becf70ab834a6af2afd835a1d59fbea38eb3a4ef
Emildawood123 <edawood997@gmail.com>
2024-03-19 11:15:36 -0400

    ssss

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
 """minOperations functions"""
-import math
-
+from sympy import isprime
 
 def minOperations(n):
     """minOperations functions"""
     if n <= 1:
         return 0
-    i = 3
+    i = 0
     while i < n:
-        if n % i == 0:
+        if not isprime(i):
+            pass
+        elif n % i == 0:
             remind = i
-        i += 2
+        i += 1
     try:
         if remind:
             pass
     except Exception:
         return n
-    while remind % 3 == 0 and remind / 3 != 1:
-        remind = remind / 3
     return int((n / remind) + remind)
+
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
