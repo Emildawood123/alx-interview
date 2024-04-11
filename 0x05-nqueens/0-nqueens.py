d62a95c2d603c697491af8a7cbaa166962a7cba9
Emildawood123 <66915790+Emildawood123@users.noreply.github.com>
2024-04-11 02:21:21 +0200

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,10 +3,12 @@
 class GfG:
     """GfG class"""
     def __init__(self):
+        """init method"""
         self.MAX = 10
         self.arr = [0] * self.MAX
 
     def canPlace(self, k, i):
+        """canPlace method"""
         for j in range(1, k):
             if (self.arr[j] == i or
                (abs(self.arr[j] - i) == abs(j - k))):
@@ -14,6 +16,7 @@ class GfG:
         return True
  
     def display(self, n):
+        """display method"""
         arr = []
         for i in range(1, n + 1):
             for j in range(1, n + 1):
@@ -24,6 +27,7 @@ class GfG:
         print(arr)
  
     def nQueens(self, k, n):
+        """nQueens"""
         for i in range(1, n + 1):
             if self.canPlace(k, i):
                 self.arr[k] = i
