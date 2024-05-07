b12c4398fd219a475a71edc035532da80d947532
Emildawood123 <edawood997@gmail.com>
2024-05-07 07:03:51 -0400

    if has no angles

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,10 +8,6 @@ def island_perimeter(grid):
     for row in range(len(grid)):
         for col in range(len(grid[row])):
             if grid[row][col] == 1:
-                if row == 0 or col == 0:
-                    return 0
-                if row == len(grid) - 1 or col == len(grid[row]) - 1:
-                    return 0
                 if grid[row - 1][col] == 0:
                     res += 1
                 if grid[row][col - 1] == 0:
@@ -20,6 +16,9 @@ def island_perimeter(grid):
                     res += 1
                 if grid[row][col + 1] == 0:
                     res += 1
+                if grid[row][col + 1] == 1 and grid[row + 1][col] == 1:
+                    if grid[row - 1][col] == 1 and grid[row][col - 1] == 1:
+                        res += 1
                 if grid[row][col + 1] == 0 and grid[row + 1][col] == 0:
                     return res
     return 0
