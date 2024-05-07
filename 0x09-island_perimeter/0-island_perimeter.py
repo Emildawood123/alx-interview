6db67a24f9a2eafdc3464406a7c01c5c3a8ae947
Emildawood123 <edawood997@gmail.com>
2024-05-07 07:19:52 -0400

    re

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,10 +8,6 @@ def island_perimeter(grid):
     for row in range(len(grid)):
         for col in range(len(grid[row])):
             if grid[row][col] == 1:
-                if grid[row - 1][col + 1] == 1 or grid[row - 1][col - 1] == 1:
-                    return 0
-                if grid[row + 1][col + 1] == 1 or grid[row + 1][col - 1] == 1:
-                    return 0
                 if grid[row - 1][col] == 0:
                     res += 1
                 if grid[row][col - 1] == 0:
