78a26aa1026720ef70946c98c92dbf0a403ad750
Emildawood123 <edawood997@gmail.com>
2024-05-07 07:27:31 -0400

    re the algo

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,11 +8,12 @@ def island_perimeter(grid):
     for row in range(len(grid)):
         for col in range(len(grid[row])):
             if grid[row][col] == 1:
-                res += 4
-                if row > 0 and grid[row - 1][col] == 1:
-                    res -= 2
-                if col > 0 and grid[row][col - 1] == 1:
-                    res -= 2
-                if grid[row][col + 1] == 0 and grid[row + 1][col] == 0:
-                    return res
-    return 0
+                if grid[row - 1][col] == 0:
+                    res += 1
+                if grid[row][col - 1] == 0:
+                    res += 1
+                if grid[row + 1][col] == 0:
+                    res += 1
+                if grid[row][col + 1] == 0:
+                    res += 1
+    return res
