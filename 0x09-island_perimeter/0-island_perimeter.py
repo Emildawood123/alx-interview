d10cdb524d8aa1ac45f944ccdf8358f3fc1c9dc5
Emildawood123 <edawood997@gmail.com>
2024-05-07 06:39:12 -0400

    delete variable unused

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,7 +8,6 @@ def island_perimeter(grid):
     for row in range(len(grid)):
         for col in range(len(grid[row])):
             if grid[row][col] == 1:
-                angles = 0
                 if grid[row - 1][col] == 0:
                     res += 1
                 if grid[row][col - 1] == 0:
