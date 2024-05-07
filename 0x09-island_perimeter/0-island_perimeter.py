aa061fa7d6fd06d94eb747af84cb43c4d54c9726
Emildawood123 <edawood997@gmail.com>
2024-05-07 07:25:53 -0400

    lash change

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,9 +9,9 @@ def island_perimeter(grid):
         for col in range(len(grid[row])):
             if grid[row][col] == 1:
                 res += 4
-                if grid[row - 1][col] == 1:
+                if row > 0 and grid[row - 1][col] == 1:
                     res -= 2
-                if grid[row][col - 1] == 1:
+                if col > 0 and grid[row][col - 1] == 1:
                     res -= 2
                 if grid[row][col + 1] == 0 and grid[row + 1][col] == 0:
                     return res
