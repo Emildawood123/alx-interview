6241cde551bf48e8b26123327fe935d16b13e6e0
Emildawood123 <edawood997@gmail.com>
2024-05-07 07:30:19 -0400

    try

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-"""moudle"""
+"""Island Perimeter"""
 
 
 def island_perimeter(grid):
-    """island_perimeter"""
-    res = 0
-    for row in range(len(grid)):
-        for col in range(len(grid[row])):
-            if grid[row][col] == 1:
-                if grid[row - 1][col] == 0:
-                    res += 1
-                if grid[row][col - 1] == 0:
-                    res += 1
-                if grid[row + 1][col] == 0:
-                    res += 1
-                if grid[row][col + 1] == 0:
-                    res += 1
-    return res
+    """Returns the perimeter of the island described in grid"""
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                cnt = 0
+                if j > 0 and grid[i][j - 1] == 1:
+                    cnt += 1
+                if j < len(grid[i]) - 1 and grid[i][j + 1] == 1:
+                    cnt += 1
+                if i > 0 and grid[i - 1][j] == 1:
+                    cnt += 1
+                if i < len(grid) - 1 and grid[i + 1][j] == 1:
+                    cnt += 1
+                perimeter += 4 - cnt
+    return perimeter
